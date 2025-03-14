6554a8c2fab3e8f336916672f0f8eec2cfe9ec79
Azim-Sabbir <mohammadsabbir044@gmail.com>
2025-03-14 15:53:24 +0600

    auth session wise header modified

--- resources/views/poll.blade.php
+++ resources/views/poll.blade.php
@@ -1,6 +1,8 @@
 <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<div id="app"></div>
+<x-app-layout>
+    <div id="app"></div>
+</x-app-layout>
 
 @viteReactRefresh
 @vite('resources/js/app.jsx')
