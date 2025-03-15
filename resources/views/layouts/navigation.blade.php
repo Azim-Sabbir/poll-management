2e2ede8bab0999d31cb659010c8e020a2ab1769f
Azim-Sabbir <mohammadsabbir044@gmail.com>
2025-03-15 13:32:09 +0600

    register option added in logged out state

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -77,6 +77,10 @@
                     <x-nav-link :href="route('login')">
                         {{ __('Login') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('register')">
+                        {{ __('Register') }}
+                    </x-nav-link>
                 </div>
             @endguest
 
