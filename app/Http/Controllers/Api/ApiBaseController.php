f9f6046ebdfcf8e073c5d49c3065ea3e6dc6ff88
Azim-Sabbir <mohammadsabbir044@gmail.com>
2025-03-20 22:14:00 +0600

    bug cookie not setting on prod

--- app/Http/Controllers/Api/ApiBaseController.php
+++ app/Http/Controllers/Api/ApiBaseController.php
@@ -55,7 +55,7 @@ class ApiBaseController extends Controller
         $response = [
             "status" => 'error',
             "message" => $message,
-            "errors" => !blank($errors) ? $errors->getMessage() : []
+            "errors" => !blank($errors) ? $errors : []
         ];
 
         return response()->json($response, $statusCode);
