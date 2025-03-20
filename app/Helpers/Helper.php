6f627448b95c724384af5f9174c5f98037ada59c
Azim-Sabbir <mohammadsabbir044@gmail.com>
2025-03-20 22:21:05 +0600

    bug cookie not setting on prod

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,6 +1,6 @@
 <?php
 if (!function_exists('getUserAgent')) {
-     function getUserAgent($request)
+    function getUserAgent($request)
     {
         $user_agent = $request->header('User-Agent');
 
@@ -9,50 +9,32 @@ if (!function_exists('getUserAgent')) {
 
         if (preg_match('/linux/i', $user_agent)) {
             $platform = 'linux';
-        }
-        elseif (preg_match('/macintosh|mac os x/i', $user_agent)) {
+        } elseif (preg_match('/macintosh|mac os x/i', $user_agent)) {
             $platform = 'mac';
-        }
-        elseif (preg_match('/windows|win32/i', $user_agent)) {
+        } elseif (preg_match('/windows|win32/i', $user_agent)) {
             $platform = 'windows';
         }
 
-        echo $platform;
+        // Store platform info in an array instead of echoing it
+        $browserData = [
+            'platform' => $platform,
+            'browser' => 'Unknown'
+        ];
 
-        echo "<br>";
-
-
-        if(preg_match('/MSIE/i',$user_agent) && !preg_match('/Opera/i',$user_agent))
-        {
-            $bname = 'Internet Explorer';
-            $ub = "MSIE";
-        }
-        elseif(preg_match('/Firefox/i',$user_agent))
-        {
-            $bname = 'Mozilla Firefox';
-            $ub = "Firefox";
-        }
-        elseif(preg_match('/Chrome/i',$user_agent))
-        {
-            $bname = 'Google Chrome';
-            $ub = "Chrome";
-        }
-        elseif(preg_match('/Safari/i',$user_agent))
-        {
-            $bname = 'Apple Safari';
-            $ub = "Safari";
-        }
-        elseif(preg_match('/Opera/i',$user_agent))
-        {
-            $bname = 'Opera';
-            $ub = "Opera";
-        }
-        elseif(preg_match('/Netscape/i',$user_agent))
-        {
-            $bname = 'Netscape';
-            $ub = "Netscape";
+        if (preg_match('/MSIE/i', $user_agent) && !preg_match('/Opera/i', $user_agent)) {
+            $browserData['browser'] = 'Internet Explorer';
+        } elseif (preg_match('/Firefox/i', $user_agent)) {
+            $browserData['browser'] = 'Mozilla Firefox';
+        } elseif (preg_match('/Chrome/i', $user_agent)) {
+            $browserData['browser'] = 'Google Chrome';
+        } elseif (preg_match('/Safari/i', $user_agent)) {
+            $browserData['browser'] = 'Apple Safari';
+        } elseif (preg_match('/Opera/i', $user_agent)) {
+            $browserData['browser'] = 'Opera';
+        } elseif (preg_match('/Netscape/i', $user_agent)) {
+            $browserData['browser'] = 'Netscape';
         }
 
-        return $bname;
+        return $browserData["browser"];
     }
 }
